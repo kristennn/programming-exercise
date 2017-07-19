2a1008997bc1bb658965d923a8808f2cf9c255d1
13949836783@163.com <13949836783@163.com>
2017-07-19 15:23:48 +0800

    complete 06-interger-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -4,7 +4,20 @@
 print "请输入一个整数，然后按 Enter: "
 x = gets
 
-# ....
+a = x.to_i
+if a == 0
+  result = "零"
+elsif a > 0
+  result = "正数"
+else
+  result = "负数"
+end
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+if a % 2 == 0
+  m = "偶数"
+else
+  m = "奇数"
+end
+
+puts "这个数是___#{result}__ (正数或零或负数)"
+puts "这个数是__#{m}___ (偶数或奇数)"
