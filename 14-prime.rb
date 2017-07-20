67d73c9f679fc1c961b4645e4ebf00ccbb3b3cb6
13949836783@163.com <13949836783@163.com>
2017-07-20 11:05:17 +0800

    complete 14-prime

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,7 +1,14 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+  ary = (2...n).to_a
+  ary.each do |i|
+    if n % i != 0
+      return true
+    else
+      return false
+    end
+  end
 end
 
 print "请输入数字 N，然后按 Enter: "
