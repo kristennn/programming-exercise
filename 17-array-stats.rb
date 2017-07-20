9de4d252f48ab8c4f29b2d723f34c17e8f9d1c0c
13949836783@163.com <13949836783@163.com>
2017-07-20 11:42:47 +0800

    complete 17-array-stats

--- 17-array-stats.rb
+++ 17-array-stats.rb
@@ -1,6 +1,7 @@
 # 使用者不断输入数字存进 Array，最后输出总和、平均、最大值、最小值
 
 arr = []
+sum = 0
 
 while (true)
   print "请输入数字，结束请直接按 Enter: "
@@ -9,12 +10,16 @@ while (true)
     break
   else
     arr << user_input.to_i
+    arr.each do |i|
+      sum += i
+    end
+    avg = sum / arr.length
   end
 end
 
 puts arr.to_s
 
-puts "总和是 _____"
-puts "平均是 _____"
-puts "最大值是 _____"
-puts "最小值是 _____"
+puts "总和是 __#{sum}___"
+puts "平均是 __#{avg}___"
+puts "最大值是 __#{arr.max}___"
+puts "最小值是 __#{arr.min}___"
